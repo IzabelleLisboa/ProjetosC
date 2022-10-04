a4d4f929f44a0dd8e22e9d6a961fe7f447c19547
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-10-04 11:03:58 +0100

    troca palavras

--- Aula3/main.c
+++ Aula3/main.c
@@ -5,9 +5,9 @@
 int main()
 {
     setlocale(LC_ALL,"Portuguese");
-    int qc;
+    /*int qc;
     char nome[15]= "",nome2[12]="";
-    /*printf("Insira o seu Nome que deseja calcular o número de caracters: ");
+    printf("Insira o seu Nome que deseja calcular o número de caracters: ");
     scanf("%s", &nome);
     strlen(nome);
     printf("\nQuantidade de caracters do nome%d",nome);
@@ -23,13 +23,27 @@ int main()
     qc=strlen(nome);
     printf("nome %s tem %d caracters.",nome,qc);*/
 
-    char palavra1[20],palavra2[20];
-    printf("Insira a palavra:");
+    /*char palavra1[20],palavra2[20];
+    printf("Insira a palavra1:");
     scanf("%s", &palavra1);
     printf("Insira a palavra2:");
     scanf("%s", &palavra2);
     strcpy(palavra1,palavra2);
-    printf("palavra 1 é %s e o palavra é %s", palavra1,palavra2);
+    printf("palavra 1:%s ",palavra1);
+    printf("palavra2 %s",palavra2);*/
+
+    char palavraA[20],palavraB[20],palavraC[20];
+    printf("Insira a palavra1:");
+    scanf("%s", &palavraA);
+    printf("Insira a palavra2:");
+    scanf("%s", &palavraB);
+    strcpy(palavraC,palavraA);
+    strcpy(palavraA,palavraB);
+    strcpy(palavraB,palavraC);
+    printf("palavra 1:%s ",palavraA);
+    printf("palavra 2 %s",palavraB);
+
+
 
     /*float area, raio;
     printf("Raio da circunferencia? ");
