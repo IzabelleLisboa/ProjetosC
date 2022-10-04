734fdb48a4b5b8c95d5b2de0edeed8a8043a528b
Izabelle Lisboa <izabellelimalisboa10@gmail.com>
2022-10-04 10:26:16 +0100

    nome e palvra

--- Aula3/main.c
+++ Aula3/main.c
@@ -5,14 +5,39 @@
 int main()
 {
     setlocale(LC_ALL,"Portuguese");
-    float area, raio;
+    int qc;
+    char nome[15]= "",nome2[12]="";
+    /*printf("Insira o seu Nome que deseja calcular o número de caracters: ");
+    scanf("%s", &nome);
+    strlen(nome);
+    printf("\nQuantidade de caracters do nome%d",nome);
+    printf("\nInsira o seu Nome que deseja calcular o número de caracters: ");
+    scanf("%s", &nome);
+    strlen(nome2);
+    strcpy(nome,nome2);
+    printf("\nQuantidade de caracters do nome%d",nome);*/
 
+
+   /* printf("Insira o seu Nome que deseja calcular o número de caracters:");
+    scanf("%s", &nome);
+    qc=strlen(nome);
+    printf("nome %s tem %d caracters.",nome,qc);*/
+
+    char palavra1[20],palavra2[20];
+    printf("Insira a palavra:");
+    scanf("%s", &palavra1);
+    printf("Insira a palavra2:");
+    scanf("%s", &palavra2);
+    strcpy(palavra1,palavra2);
+    printf("palavra 1 é %s e o palavra é %s", palavra1,palavra2);
+
+    /*float area, raio;
     printf("Raio da circunferencia? ");
     scanf("%f", &raio );
     area = M_PI * (raio*raio);
     printf("Circulo:");
     printf("\nRaio:%.2f",raio);
-    printf("\nÁrea: %.2f\n", area); //duas casas decimais//
+    printf("\nÁrea: %.2f\n", area); //duas casas decimais//*/
 
 
     return 0;
